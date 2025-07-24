34c23d830a62fcecc77eec96be755ac64a84cb7f
Solarin-the-Great <cavanaughmt@gmail.com>
2025-07-23 23:30:14 -0500

    Upgrade guns to triple cannon

--- shot.py
+++ shot.py
@@ -3,12 +3,16 @@ from constants import *
 from circleshape import *
 
 class Shot(CircleShape):
-    def __init__(self, x, y, radius):
+    def __init__(self, x, y, radius, velocity):
         super().__init__(
             x,
             y,
             radius,
         )
+        self.velocity = velocity
 
     def draw(self, screen):
         pygame.draw.circle(screen, "white", self.position.xy, self.radius, 2)
+
+    def update(self, dt):
+        self.position += self.velocity * dt
